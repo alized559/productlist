79c4556323484b5e6b4b2bba679f220e06d85c97
Ali Zein Al Dine <alized559@gmail.com>
2022-01-23 17:13:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import productImage from './images/car.jpg';
 import Axios from "axios";
 
 const selectedProduct = [];
+const selectedState = [];
+const selectedCity = [];
 
 function App() {
   const [activeItemIndex, setActiveItemIndex] = useState(0);
@@ -77,11 +79,60 @@ function App() {
 
   const selectState = (e) => {
     setSelectedProductState(e);
-    
+    if (selectedProduct.length > 0) {
+      selectedState.length = 0;
+      for (var i = 0; i < selectedProduct.length; i++) {
+        if (selectedProduct[i].address.state == selectedProductState) {
+          selectedState[i] = selectedProduct[i];
+        }
+      }
+    } else {
+      selectedState.length = 0;
+      for (var i = 0; i < products.length; i++) {
+        if (products[i].address.state == selectedProductState) {
+          selectedState[i] = products[i];
+        }
+      }
+    }
+  }
+
+  function getCity() {
+    return (
+      selectedState.map(name => {
+        return (
+          <Dropdown.Item eventKey={name}>{name.address.city}</Dropdown.Item>
+        );
+      })
+    )
+  }
+
+  function getAllCities() {
+    return (
+      city.map(name => {
+        return (
+          <Dropdown.Item eventKey={name}>{name}</Dropdown.Item>
+        );
+      })
+    )
   }
 
   const selectCity = (e) => {
-
+    setSelectedProductCity(e);
+    if (selectedState.length > 0) {
+      selectedCity.length = 0;
+      for (var i = 0; i < selectedState.length; i++) {
+        if (selectedState[i].address.city == selectedProductCity) {
+          selectedCity[i] = selectedState[i];
+        }
+      }
+    } else {
+      selectedCity.length = 0;
+      for (var i = 0; i < products.length; i++) {
+        if (products[i].address.city == selectedProductCity) {
+          selectedCity[i] = products[i];
+        }
+      }
+    }
   }
   
   return (
@@ -123,17 +174,19 @@ function App() {
                   </Dropdown.Menu>
                 </Dropdown>
 
-                <Dropdown>
+                <Dropdown onSelect={selectCity} value={selectedProductCity}>
                   <Dropdown.Toggle id="drpBtn">
                     <span className="drpText">City</span>
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
-                    {city.map(name => {
-                      return (
-                        <Dropdown.Item href="#">{name}</Dropdown.Item>
-                      );
-                    })}
+                    {
+                      selectedState.length > 0
+                      ?
+                      getCity()
+                      :
+                      getAllCities()
+                    }
                   </Dropdown.Menu>
                 </Dropdown>
                 <br/>
@@ -146,7 +199,7 @@ function App() {
               <h1>Edvora</h1>
               <h5 className="prodH5">Products</h5>
               {
-                selectedProduct.length != 0
+                selectedProduct.length > 0 || selectedState.length > 0 || selectedCity.length > 0
                 ?
                 singleProduct()
                 :
@@ -160,7 +213,110 @@ function App() {
   );
 
   function singleProduct() {
-    return (
+    if (selectedState.length > 0) {
+      return (
+      <div>
+        <p>{selectedProductName}</p>
+        <hr className="hr1"/>
+        <div className="list">
+          <ItemsCarousel
+            className="myCarousel"
+            requestToChangeActive={setActiveItemIndex}
+            activeItemIndex={activeItemIndex}
+            numberOfCards={4}
+            gutter={20}
+            leftChevron={<button className="ind">{'<'}</button>}
+            rightChevron={<button className="ind1">{'>'}</button>}
+            outsideChevron
+            chevronWidth={chevronWidth}
+          >
+        {
+          selectedState.map(product => {
+            return (
+              <div className="listCard">
+                <Row>
+                  <Col sm={5}>
+                    <img src={product.image} alt="product_image"/>
+                  </Col>
+
+                  <Col sm={7}>
+                    <p>{product.product_name}<br/><span className="span">{product.brand_name}</span><br/>$ {product.price}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p className="span1">{product.address.state}, {product.address.city}</p>
+                  </Col>
+                  <Col sm={7}>
+                    <p className="span1">{product.date}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p className="span">{product.discription}</p>
+                  </Col>
+                </Row>
+              </div>
+            )
+          })
+        }
+        </ItemsCarousel>
+      </div>
+      </div>
+    )
+    } else if (selectedCity.length > 0) {
+      return (
+      <div>
+        <p>{selectedProductName}</p>
+        <hr className="hr1"/>
+        <div className="list">
+          <ItemsCarousel
+            className="myCarousel"
+            requestToChangeActive={setActiveItemIndex}
+            activeItemIndex={activeItemIndex}
+            numberOfCards={4}
+            gutter={20}
+            leftChevron={<button className="ind">{'<'}</button>}
+            rightChevron={<button className="ind1">{'>'}</button>}
+            outsideChevron
+            chevronWidth={chevronWidth}
+          >
+        {
+          selectedCity.map(product => {
+            return (
+              <div className="listCard">
+                <Row>
+                  <Col sm={5}>
+                    <img src={product.image} alt="product_image"/>
+                  </Col>
+
+                  <Col sm={7}>
+                    <p>{product.product_name}<br/><span className="span">{product.brand_name}</span><br/>$ {product.price}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p className="span1">{product.address.state}, {product.address.city}</p>
+                  </Col>
+                  <Col sm={7}>
+                    <p className="span1">{product.date}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p className="span">{product.discription}</p>
+                  </Col>
+                </Row>
+              </div>
+            )
+          })
+        }
+        </ItemsCarousel>
+      </div>
+      </div>
+    )
+    } else {
+      return (
       <div>
         <p>{selectedProductName}</p>
         <hr className="hr1"/>
@@ -210,66 +366,171 @@ function App() {
       </div>
       </div>
     )
+    }
   }
 
   function product() {
-    return (
-      productName.map(name => {
-        return (
-          <div>
-            <p>{name}</p>
-            <hr className="hr1"/>
-            <div className="list">
-              <ItemsCarousel
-                className="myCarousel"
-                requestToChangeActive={setActiveItemIndex}
-                activeItemIndex={activeItemIndex}
-                numberOfCards={4}
-                gutter={20}
-                leftChevron={<button className="ind">{'<'}</button>}
-                rightChevron={<button className="ind1">{'>'}</button>}
-                outsideChevron
-                chevronWidth={chevronWidth}
-              >
-                {
-                  products.map(product => {
-                    if (product.product_name == name) {
-                      return(
-                        <div className="listCard">
-                          <Row>
-                            <Col sm={5}>
-                              <img src={product.image} alt="product_image"/>
-                            </Col>
+    if (selectedState.length > 0) {
+      return (
+      <div>
+        <p>{selectedProductName}</p>
+        <hr className="hr1"/>
+        <div className="list">
+          <ItemsCarousel
+            className="myCarousel"
+            requestToChangeActive={setActiveItemIndex}
+            activeItemIndex={activeItemIndex}
+            numberOfCards={4}
+            gutter={20}
+            leftChevron={<button className="ind">{'<'}</button>}
+            rightChevron={<button className="ind1">{'>'}</button>}
+            outsideChevron
+            chevronWidth={chevronWidth}
+          >
+        {
+          selectedState.map(product => {
+            return (
+              <div className="listCard">
+                <Row>
+                  <Col sm={5}>
+                    <img src={product.image} alt="product_image"/>
+                  </Col>
 
-                            <Col sm={7}>
-                              <p>{product.product_name}<br/><span className="span">{product.brand_name}</span><br/>$ {product.price}</p>
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col>
-                              <p className="span1">{product.address.state}, {product.address.city}</p>
-                            </Col>
-                            <Col sm={7}>
-                              <p className="span1">{product.date}</p>
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col>
-                              <p className="span">{product.discription}</p>
-                            </Col>
-                          </Row>
-                        </div>
-                      )
-                    }
-                  })
-                }
-              </ItemsCarousel>
-            </div>
-            <br/>
-          </div>
-        )
-      })
+                  <Col sm={7}>
+                    <p>{product.product_name}<br/><span className="span">{product.brand_name}</span><br/>$ {product.price}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p className="span1">{product.address.state}, {product.address.city}</p>
+                  </Col>
+                  <Col sm={7}>
+                    <p className="span1">{product.date}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p className="span">{product.discription}</p>
+                  </Col>
+                </Row>
+              </div>
+            )
+          })
+        }
+        </ItemsCarousel>
+      </div>
+      </div>
     )
+    } else if (selectedCity.length > 0) {
+      return (
+      <div>
+        <p>{selectedProductName}</p>
+        <hr className="hr1"/>
+        <div className="list">
+          <ItemsCarousel
+            className="myCarousel"
+            requestToChangeActive={setActiveItemIndex}
+            activeItemIndex={activeItemIndex}
+            numberOfCards={4}
+            gutter={20}
+            leftChevron={<button className="ind">{'<'}</button>}
+            rightChevron={<button className="ind1">{'>'}</button>}
+            outsideChevron
+            chevronWidth={chevronWidth}
+          >
+        {
+          selectedCity.map(product => {
+            return (
+              <div className="listCard">
+                <Row>
+                  <Col sm={5}>
+                    <img src={product.image} alt="product_image"/>
+                  </Col>
+
+                  <Col sm={7}>
+                    <p>{product.product_name}<br/><span className="span">{product.brand_name}</span><br/>$ {product.price}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p className="span1">{product.address.state}, {product.address.city}</p>
+                  </Col>
+                  <Col sm={7}>
+                    <p className="span1">{product.date}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p className="span">{product.discription}</p>
+                  </Col>
+                </Row>
+              </div>
+            )
+          })
+        }
+        </ItemsCarousel>
+      </div>
+      </div>
+    )
+    } else {
+      return (
+        productName.map(name => {
+          return (
+            <div>
+              <p>{name}</p>
+              <hr className="hr1"/>
+              <div className="list">
+                <ItemsCarousel
+                  className="myCarousel"
+                  requestToChangeActive={setActiveItemIndex}
+                  activeItemIndex={activeItemIndex}
+                  numberOfCards={4}
+                  gutter={20}
+                  leftChevron={<button className="ind">{'<'}</button>}
+                  rightChevron={<button className="ind1">{'>'}</button>}
+                  outsideChevron
+                  chevronWidth={chevronWidth}
+                >
+                  {
+                    products.map(product => {
+                      if (product.product_name == name) {
+                        return(
+                          <div className="listCard">
+                            <Row>
+                              <Col sm={5}>
+                                <img src={product.image} alt="product_image"/>
+                              </Col>
+
+                              <Col sm={7}>
+                                <p>{product.product_name}<br/><span className="span">{product.brand_name}</span><br/>$ {product.price}</p>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col>
+                                <p className="span1">{product.address.state}, {product.address.city}</p>
+                              </Col>
+                              <Col sm={7}>
+                                <p className="span1">{product.date}</p>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col>
+                                <p className="span">{product.discription}</p>
+                              </Col>
+                            </Row>
+                          </div>
+                        )
+                      }
+                    })
+                  }
+                </ItemsCarousel>
+              </div>
+              <br/>
+            </div>
+          )
+        })
+      )
+    }
   }
 }
 
